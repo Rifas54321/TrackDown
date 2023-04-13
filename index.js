f0cb51802ddac5759de0ce28716b4c3f95cad497
Rifas54321 <93052567+Rifas54321@users.noreply.github.com>
2023-04-14 02:26:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://trackdown-production.up.railway.app/";
+var hostURL="https://trackdown.onrender.com";
 //TOGGLE for Shorters
 var use1pt=true;
 
